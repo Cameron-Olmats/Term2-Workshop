bb9c031fb6d46ac8cb44776e451eaf6b1537b3fc
Cameron Olmats <cameron.olmats@edu.sait.ca>
2023-07-10 13:15:17 -0600

    Set up the project
    -installed EntityFrameworkCoreSsqlServer,
     EntityFrameworkCore.Tools, and System.Configuration.ConfigurationManager on both projects.
    -Merged the database using the scaffold string.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Term2-Workshop
 Group 4 Workshop 4
+
+TravelExpertsApp contains the solution file aswell as the project folders.
+
+I coopied a .gitignore from github. Hopefully it works.
---
+++ TravelExpertsApp/TravelExpertsGUI/frmTravelExperts.cs
@@ -0,0 +1,10 @@
+namespace TravelExpertsGUI
+{
+    public partial class frmTravelExperts : Form
+    {
+        public frmTravelExperts()
+        {
+            InitializeComponent();
+        }
+    }
+}
