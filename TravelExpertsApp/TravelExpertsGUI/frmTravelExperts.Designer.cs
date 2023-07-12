1b336508954002380a1520b889944c7df181bc76
Cameron Olmats <cameron.olmats@edu.sait.ca>
2023-07-12 13:57:22 -0600

    Blocking out test ideas
    Made it so that the data displayed changes when you click on the corresponding button. The add buttons will also open the matching form.

--- TravelExpertsApp/TravelExpertsGUI/frmTravelExperts.Designer.cs
+++ TravelExpertsApp/TravelExpertsGUI/frmTravelExperts.Designer.cs
@@ -28,18 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnTravelPackage = new Button();
+            btnProducts = new Button();
+            btnSuppliers = new Button();
+            dgvMain = new DataGridView();
+            groupBox1 = new GroupBox();
+            btnModify = new Button();
+            btnRemove = new Button();
+            btnAdd = new Button();
+            btnExit = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvMain).BeginInit();
+            groupBox1.SuspendLayout();
             SuspendLayout();
+            // 
+            // btnTravelPackage
+            // 
+            btnTravelPackage.Location = new Point(12, 31);
+            btnTravelPackage.Name = "btnTravelPackage";
+            btnTravelPackage.Size = new Size(200, 26);
+            btnTravelPackage.TabIndex = 0;
+            btnTravelPackage.Text = "Edit Travel Packages";
+            btnTravelPackage.UseVisualStyleBackColor = true;
+            btnTravelPackage.Click += btnTravelPackage_Click;
+            // 
+            // btnProducts
+            // 
+            btnProducts.Location = new Point(12, 79);
+            btnProducts.Name = "btnProducts";
+            btnProducts.Size = new Size(75, 23);
+            btnProducts.TabIndex = 1;
+            btnProducts.Text = "Products";
+            btnProducts.UseVisualStyleBackColor = true;
+            btnProducts.Click += btnProducts_Click;
+            // 
+            // btnSuppliers
+            // 
+            btnSuppliers.Location = new Point(137, 79);
+            btnSuppliers.Name = "btnSuppliers";
+            btnSuppliers.Size = new Size(75, 23);
+            btnSuppliers.TabIndex = 2;
+            btnSuppliers.Text = "Suppliers";
+            btnSuppliers.UseVisualStyleBackColor = true;
+            btnSuppliers.Click += btnSuppliers_Click;
+            // 
+            // dgvMain
+            // 
+            dgvMain.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvMain.Location = new Point(30, 22);
+            dgvMain.Name = "dgvMain";
+            dgvMain.RowTemplate.Height = 25;
+            dgvMain.Size = new Size(439, 339);
+            dgvMain.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnModify);
+            groupBox1.Controls.Add(btnRemove);
+            groupBox1.Controls.Add(btnAdd);
+            groupBox1.Controls.Add(dgvMain);
+            groupBox1.Location = new Point(225, 9);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(582, 457);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // btnModify
+            // 
+            btnModify.Location = new Point(138, 414);
+            btnModify.Name = "btnModify";
+            btnModify.Size = new Size(75, 23);
+            btnModify.TabIndex = 6;
+            btnModify.Text = "Modify";
+            btnModify.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            btnRemove.Location = new Point(240, 414);
+            btnRemove.Name = "btnRemove";
+            btnRemove.Size = new Size(75, 23);
+            btnRemove.TabIndex = 5;
+            btnRemove.Text = "Remove";
+            btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(30, 414);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(75, 23);
+            btnAdd.TabIndex = 4;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(713, 492);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(75, 23);
+            btnExit.TabIndex = 5;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
             // 
             // frmTravelExperts
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            ClientSize = new Size(819, 527);
+            Controls.Add(btnExit);
+            Controls.Add(groupBox1);
+            Controls.Add(btnSuppliers);
+            Controls.Add(btnProducts);
+            Controls.Add(btnTravelPackage);
+            Margin = new Padding(2, 1, 2, 1);
             Name = "frmTravelExperts";
             Text = "Travel Experts";
+            Load += frmTravelExperts_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvMain).EndInit();
+            groupBox1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnTravelPackage;
+        private Button btnProducts;
+        private Button btnSuppliers;
+        private DataGridView dgvMain;
+        private GroupBox groupBox1;
+        private Button btnModify;
+        private Button btnRemove;
+        private Button btnAdd;
+        private Button btnExit;
     }
 }
--- TravelExpertsApp/TravelExpertsGUI/frmTravelExperts.cs
+++ TravelExpertsApp/TravelExpertsGUI/frmTravelExperts.cs
@@ -1,10 +1,85 @@
+/*
+ * This form acts as a menu where you can select and access the different functions of the application
+ * 
+ * Author: Cameron Olmats
+ * Date created: 7/10/2023
+ */
+
+using TravelExpertsData;
+
 namespace TravelExpertsGUI
 {
     public partial class frmTravelExperts : Form
     {
+        private string tableSelection = "";
         public frmTravelExperts()
         {
             InitializeComponent();
+        }
+
+        private void frmTravelExperts_Load(object sender, EventArgs e)
+        {
+            //using (TravelExpertsContext db = new TravelExpertsContext())
+            //{
+            //    //dataGridView1.DataSource = db.Customers.ToList();
+            //}
+        }
+
+
+        //When the TravelPackage, Products, and supplier buttons are clicked:
+        //The data grid view updates it's data and the buttons are chagned to match the selected button
+        private void btnTravelPackage_Click(object sender, EventArgs e)
+        {
+            
+            tableSelection = "Packages";
+            using (TravelExpertsContext db = new TravelExpertsContext())
+            {
+                dgvMain.DataSource = db.Packages.ToList();
+            }
+
+        }
+
+        private void btnProducts_Click(object sender, EventArgs e)
+        {
+            tableSelection = "Products";
+            using (TravelExpertsContext db = new TravelExpertsContext())
+            {
+                dgvMain.DataSource = db.Products.ToList();
+            }
+        }
+
+        private void btnSuppliers_Click(object sender, EventArgs e)
+        {
+            tableSelection = "Suppliers";
+            using (TravelExpertsContext db = new TravelExpertsContext())
+            {
+                dgvMain.DataSource = db.Suppliers.ToList();
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            switch (tableSelection)
+            {
+                case "Packages":
+                    {
+                        frmPackages secondForm = new frmPackages();
+                        DialogResult result = secondForm.ShowDialog();
+                        break;
+                    }
+                    
+                //case "Products":
+                //    frmProducts secondForm = new frmProducts();
+                //    DialogResult result = secondForm.ShowDialog();
+                //    break;
+                case "Suppliers":
+                    {
+                        frmSuppliers secondForm = new frmSuppliers();
+                        DialogResult result = secondForm.ShowDialog();
+                        break;
+                    }
+                    
+            }
         }
     }
 }
