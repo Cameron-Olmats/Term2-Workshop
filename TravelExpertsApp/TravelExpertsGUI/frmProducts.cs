8799f7131089bdbb4b8bfe30223c98131c97e6f2
nancex <bubblez.me6@gmail.com>
2023-08-03 13:59:00 -0600

    Added functionality

--- TravelExpertsApp/TravelExpertsGUI/frmProducts.cs
+++ TravelExpertsApp/TravelExpertsGUI/frmProducts.cs
@@ -86,6 +86,17 @@ namespace TravelExpertsGUI
                 // Reload products when the modification is successful
                 LoadProducts();
             }
+
+            // Disable the control box (minimize, maximize, close buttons) of the frmProducts form
+            this.ControlBox = false;
+
+            // Enable and disable the necessary buttons
+            btnSubmit.Enabled = false;
+            btnModify.Enabled = true;
+            btnDelete.Enabled = false;
+
+            // Allow the user to modify the product name
+            txtProdName.ReadOnly = false;
         }
 
         private void btnSubmit_Click(object sender, EventArgs e)
@@ -150,5 +161,5 @@ namespace TravelExpertsGUI
             }
         }
     }
-}
+ }
 
