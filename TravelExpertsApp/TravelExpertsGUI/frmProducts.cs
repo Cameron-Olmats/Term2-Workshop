354c469b18cc6fb94daebb22d1340e64f6784a9a
nancex <bubblez.me6@gmail.com>
2023-08-03 21:50:46 -0600

    Updates

--- TravelExpertsApp/TravelExpertsGUI/frmProducts.cs
+++ TravelExpertsApp/TravelExpertsGUI/frmProducts.cs
@@ -20,162 +20,170 @@ using static System.Net.Mime.MediaTypeNames;
  */
 namespace TravelExpertsGUI
 {
-    public partial class frmProducts : Form
-    {
-        private List<Product> products;
-        public Product SelectedProd; // To track the selected product in the DataGridView
-        private Product modifiedProduct; // To track the modified product in the DataGridView
+	public partial class frmProducts : Form
+	{
+		private List<Product> products;
+		public Product SelectedProd; // To track the selected product in the DataGridView
+		private Product modifiedProduct; // To track the modified product in the DataGridView
 
-        public frmProducts()
-        {
-            InitializeComponent();
-        }
+		public frmProducts()
+		{
+			InitializeComponent();
+		}
 
-        public frmProducts(Product selectedProd)
-        {
-            SelectedProd = selectedProd;
-            InitializeComponent();
-        }
+		public frmProducts(Product selectedProd)
+		{
+			SelectedProd = selectedProd;
+			InitializeComponent();
+		}
 
-        //public class Product
-        //{
-        //    public int ProdID { get; set; }
-        //    public string ProdName { get; set; }
-        //    public string ProdDesc { get; set; }
-        //    public decimal BasePrice { get; set; }
-        //    public decimal AgencyCommission { get; set; }
-        //}
+		//public class Product
+		//{
+		//    public int ProdID { get; set; }
+		//    public string ProdName { get; set; }
+		//    public string ProdDesc { get; set; }
+		//    public decimal BasePrice { get; set; }
+		//    public decimal AgencyCommission { get; set; }
+		//}
 
-        private void LoadProducts()
-        {
-            // Fetch products from the database and populate the data grid view
-            using (var dbContext = new TravelExpertsContext())
-            {
-                products = dbContext.Products.ToList();
-            }
+		private void LoadProducts()
+		{
+			// Fetch products from the database and populate the data grid view
+			using (var dbContext = new TravelExpertsContext())
+			{
+				products = dbContext.Products.ToList();
+			}
 
-            dataGView_Prods.DataSource = products;
-        }
+			dataGView_Prods.DataSource = products;
+		}
 
-        private void frmProducts_Load(object sender, EventArgs e)
-        {
-            LoadProducts();
-        }
+		private void frmProducts_Load(object sender, EventArgs e)
+		{
+			dataGView_Prods.ClearSelection(); // Clear the selection before loading products
+			LoadProducts();
+		}
 
-        private void dataGView_Prods_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            if (e.RowIndex >= 0 && e.RowIndex < products.Count)
-            {
-                // Get the selected product from the data grid view
-                modifiedProduct = products[e.RowIndex];
+		private void dataGView_Prods_CellClick(object sender, DataGridViewCellEventArgs e)
+		{
+			if (e.RowIndex >= 0 && e.RowIndex < products.Count)
+			{
+				// Get the selected product from the data grid view
+				modifiedProduct = products[e.RowIndex];
 
-                // Display the selected product's details in the text boxes
-                txtProdID.Text = modifiedProduct.ProductId.ToString();
-                txtProdName.Text = modifiedProduct.ProdName;
-            }
-        }
+				// Display the selected product's details in the text boxes
+				txtProdID.Text = modifiedProduct.ProductId.ToString();
+				txtProdName.Text = modifiedProduct.ProdName;
+			}
+		}
 
-        private void btnModify_Click(object sender, EventArgs e)
-        {
-            if (modifiedProduct == null)
-            {
-                MessageBox.Show("Please select a product to modify.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
+		private void btnModify_Click(object sender, EventArgs e)
+		{
+			if (modifiedProduct == null)
+			{
+				MessageBox.Show("Please select a product to modify.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
 
-            ModifyProduct();
-        }
-        
-            //if (result == DialogResult.OK)
-            //{
-            //    // Reload products when the modification is successful
-            //    LoadProducts();
-            //}
+			string newName = txtProdName.Text;
+			if (string.IsNullOrWhiteSpace(newName))
+			{
+				MessageBox.Show("Product Name cannot be empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			ModifyProduct();
+		}
+
+		//if (result == DialogResult.OK)
+		//{
+		//    // Reload products when the modification is successful
+		//    LoadProducts();
+		//}
 
 
-            //// Enable and disable the necessary buttons
-            //btnSubmit.Enabled = false;
-            //btnModify.Enabled = true;
-            //btnDelete.Enabled = false;
+		//// Enable and disable the necessary buttons
+		//btnSubmit.Enabled = false;
+		//btnModify.Enabled = true;
+		//btnDelete.Enabled = false;
 
-            //// Allow the user to modify the product name
-            //txtProdName.ReadOnly = false;
-  
+		//// Allow the user to modify the product name
+		//txtProdName.ReadOnly = false;
 
-        private void ModifyProduct()
-        {
-            using (var dbContext = new TravelExpertsContext())
-            {
-                Product currentProduct = dbContext.Products.Find(modifiedProduct.ProductId);
-                if (currentProduct != null)
-                    currentProduct.ProdName = txtProdName.Text;
-                dbContext.SaveChanges();
-            }
 
-            // Reload products when the addition is successful
-            LoadProducts();       
-        }
+		private void ModifyProduct()
+		{
+			using (var dbContext = new TravelExpertsContext())
+			{
+				Product currentProduct = dbContext.Products.Find(modifiedProduct.ProductId);
+				if (currentProduct != null)
+					currentProduct.ProdName = txtProdName.Text;
+				dbContext.SaveChanges();
+			}
 
-        private void btnSubmit_Click(object sender, EventArgs e)
-        {
-            // Gather the data from the input controls
-            string productName = txtProdName.Text;
+			// Reload products when the addition is successful
+			LoadProducts();
+		}
 
-            if (string.IsNullOrEmpty(productName))
-            {
-                MessageBox.Show("Product Name cannot be empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
+		private void btnSubmit_Click(object sender, EventArgs e)
+		{
+			// Gather the data from the input controls
+			string productName = txtProdName.Text;
 
-            // Create a new product instance and add it to the database
-            Product newProduct = new Product
-            {
-                ProdName = productName
-            };
+			if (string.IsNullOrEmpty(productName))
+			{
+				MessageBox.Show("Product Name cannot be empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
 
-            AddProduct(newProduct);
-        }
+			// Create a new product instance and add it to the database
+			Product newProduct = new Product
+			{
+				ProdName = productName
+			};
 
-        private void AddProduct(Product newProduct)
-        {
-            using (var dbContext = new TravelExpertsContext())
-            {
-                dbContext.Products.Add(newProduct);
-                dbContext.SaveChanges();
-            }
+			AddProduct(newProduct);
+		}
 
-            // Reload products when the addition is successful
-            LoadProducts();
-        }
+		private void AddProduct(Product newProduct)
+		{
+			using (var dbContext = new TravelExpertsContext())
+			{
+				dbContext.Products.Add(newProduct);
+				dbContext.SaveChanges();
+			}
 
-        private void btnDelete_Click(object sender, EventArgs e)
-        {
-            if (modifiedProduct == null)
-            {
-                MessageBox.Show("Please select a product to delete.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
+			// Reload products when the addition is successful
+			LoadProducts();
+		}
 
-            DialogResult result = MessageBox.Show("Are you sure you want to delete this product?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question); // Deletion validation
-            if (result == DialogResult.Yes)
-            {
-                using (var dbContext = new TravelExpertsContext())
-                {
-                    var existingProduct = dbContext.Products.Find(modifiedProduct.ProductId);
-                    if (existingProduct != null)
-                    {
-                        dbContext.Products.Remove(existingProduct);
-                        dbContext.SaveChanges();
-                    }
-                }
+		private void btnDelete_Click(object sender, EventArgs e)
+		{
+			if (modifiedProduct == null)
+			{
+				MessageBox.Show("Please select a product to delete.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
 
-                // Clear the selected product after deletion
-                modifiedProduct = null;
+			DialogResult result = MessageBox.Show("Are you sure you want to delete this product?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question); // Deletion validation
+			if (result == DialogResult.Yes)
+			{
+				using (var dbContext = new TravelExpertsContext())
+				{
+					var existingProduct = dbContext.Products.Find(modifiedProduct.ProductId);
+					if (existingProduct != null)
+					{
+						dbContext.Products.Remove(existingProduct);
+						dbContext.SaveChanges();
+					}
+				}
 
-                // Reload products after deletion
-                LoadProducts();
-            }
-        }
-    }
- }
+				// Clear the selected product after deletion
+				modifiedProduct = null;
+
+				// Reload products after deletion
+				LoadProducts();
+			}
+		}
+	}
+}
 
